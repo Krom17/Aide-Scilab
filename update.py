1beae58a6ff02d3ff36ca7abb80305a0e94cc93f
Christophe Saint-Jean <christophe.saint-jean@univ-lr.fr>
2014-02-03 15:17:33 +0100

    Ajout de label pour les references croisés des sujets de TP

--- update.py
+++ update.py
@@ -1,68 +1,52 @@
 #!/usr/bin/python
 
-__author__ = 'e.bidelman (Eric Bidelman)'
-
 import getopt
 import mimetypes
 import os.path
 import sys
-
 import gdata.sample_util
 import gdata.sites.client
 import gdata.sites.data
 
 
 SOURCE_APP_NAME = 'googleInc-GoogleSitesAPIPythonLibSample-v1.1'
+site_name='testpythonscilab'
+client = gdata.sites.client.SitesClient(source=SOURCE_APP_NAME, site=site_name)
+try:
+	gdata.sample_util.authorize_client(client, service=client.auth_service, source=SOURCE_APP_NAME, scopes=['http://sites.google.com/testpythonscilab/','https://sites.google.com/testpythonscilab/'])
+except gdata.client.BadAuthentication:
+	exit('Invalid user credentials given.')
+except gdata.client.Error:
+	exit('Login Error')
+	
+	
+path = '/page1'
+print 'Fetching page by its path: ' + path
+uri = '%s?path=%s' % (client.MakeContentFeedUri(), path)
+:
 
-site_name=None 
-site_domain=None
 
-client = gdata.sites.client.SitesClient(source=SOURCE_APP_NAME, site=site_name, domain=site_domain)
 
-self.client.http_client.debug = debug
+	
+	
+	
 
-    try:
-      gdata.sample_util.authorize_client(
-          self.client, service=self.client.auth_service, source=SOURCE_APP_NAME,
-          scopes=['http://sites.google.com/feeds/',
-                  'https://sites.google.com/feeds/'])
-    except gdata.client.BadAuthentication:
-      exit('Invalid user credentials given.')
-    except gdata.client.Error:
-      exit('Login Error')
-      
-      
- % choix 6
- 
-          print "\nFetching filecainets on '%s'...\n" % self.client.site
+uri = '%s?kind=%s' % (client.MakeContentFeedUri(),'filecabinet')
+feed = client.GetContentFeed(uri=uri)
+attachment = client.UploadAttachment('aide_scilab.pdf', feed.entry[3], content_type='application/pdf',
+                                     title='Aide mémoire Scilab', description='HR Packet')
+print 'Uploaded. View it at: %s' % attachment.GetAlternateLink().href
 
-          uri = '%s?kind=%s' % (self.client.make_content_feed_uri(),
-                                'filecabinet')
-          feed = self.client.GetContentFeed(uri=uri)
+ms = gdata.data.MediaSource(file_path='/path/to/replacementContent.doc', content_type='application/msword')
+existing_attachment.title.text = 'Updated Document Title'
+existing_attachment.summary.text = 'version 2.0'
 
-          selection = self.GetChoiceSelection(
-              feed, 'Select a filecabinet to upload to: ')
+updated_attachment = client.Update(existing_attachment, media_source=ms)
+print "Attachment '%s' changed to '%s'" % (existing_attachment.title.text, updated_attachment.title.text)
 
-          filepath = raw_input('Enter a filename: ')
-          page_title = raw_input('Enter a title for the file: ')
-          description = raw_input('Enter a description: ')
 
-          filename = os.path.basename(filepath)
-          file_ex = filename[filename.rfind('.'):]
-          if not file_ex in mimetypes.types_map:
-            content_type = raw_input(
-                'Unrecognized file extension. Please enter the mime type: ')
-          else:
-            content_type = mimetypes.types_map[file_ex]
 
-          entry = None
-          if selection is not None:
-            entry = feed.entry[selection - 1]
 
-          new_entry = self.client.UploadAttachment(
-              filepath, entry, content_type=content_type, title=page_title,
-              description=description)
-          print 'Uploaded. View it at: %s' % new_entry.GetAlternateLink().href
 
-     
-      
+
+
